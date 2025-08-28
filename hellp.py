897c4ba888b7beb06c344bdc562408efcc5622a4
Esteban Vallejo <56201388+valesteb@users.noreply.github.com>
2025-08-27 23:50:31 -0500

    Humble beginnings!

---
+++ hellp.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+name = input("Enter your name: ")
+print(f"Hello, {name}. Nice to meet you!")
+
+age_input = input(f"How old are you? ")
+age = int(age_input)
+bot_age = 1  #to be replaced with actual bot age at the time it's being used
+age_diff = age - bot_age
+print(f"Wow, you are {age_diff} years older than me!")
+
+color = input("What is your favorite color? ")
+print(f"I can understand why. {color} is a nice color!")
+
